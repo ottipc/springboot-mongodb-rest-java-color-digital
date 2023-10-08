e3a28cbeecf705917217771b09022e66bb657714
Ottavio Braun <ottavio.braun@googlemail.com>
2023-10-08 17:59:27 +0200

    ADD Swagger Config

---
+++ src/main/java/com/devguy/testshop/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.devguy.config.testshop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.devguy")) // Replace with your package name
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Your API Documentation")
+                .description("Description of your API")
+                .version("1.0.0")
+                .build();
+    }
+}
